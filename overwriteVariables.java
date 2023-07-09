4814a55a8802833d933d0debaf94b0158d63df11
896ma <solarcrash3@gmail.com>
2023-07-09 11:07:08 +0300

    Java Time

---
+++ overwriteVariables.java
@@ -0,0 +1,8 @@
+public class overwriteVariables{
+    public static void main(String args[]){
+       int  myNum =24;
+       myNum=35;
+       System.out.println(myNum);
+
+    }
+}
---
+++ showTime.java
@@ -0,0 +1,12 @@
+//importing time stamp
+
+import java.time.*;
+
+public class showTime{
+    public static void main(String args[]){ Instant timestamp = Instant.now();
+         System.out.println("\nCurrent Timestamp: " + timestamp+"\n"); 
+        }  
+        
+    }
+    //program start
+
